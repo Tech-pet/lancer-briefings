1315b0dee92a4049824641e1f603ee6f44f2bc20
Techpet <tom_wells@hotmail.co.uk>
2023-10-27 15:12:30 +0100

    Haypenny Image

--- src/App.vue
+++ src/App.vue
@@ -106,7 +106,7 @@ export default {
           "code": "462370be-bd0f-41c2-b667-cc75f3a59a96///NDL-C-DEEP-STATION//377308ad-ba23-410b-ae37-68a1fb5f8db4",
           "corpro": "GMS",
           "frame": "Everest",
-          "mech": "Type - Worst Beat"
+          "mech": "Sudden Intervention"
         },
         {
           "callsign": "Chutzpah",
@@ -114,31 +114,31 @@ export default {
           "code": "7cd700cc-c990-48ed-892f-e5468de724c4///NDL-C-DEEP-STATION//a98c3e28-ad4a-4f89-bcd9-501464e960da",
           "corpro": "GMS",
           "frame": "Everest",
-          "mech": "Necrophile"
+          "mech": "Golem"
         },
         {
           "callsign": "Heartstring",
           "alias": "Eva Lorn",
           "code": "4be26ce9-923b-4069-b6c9-76437d4be455///NDL-C-DEEP-STATION//056940c6-8d55-4190-8e85-57caa043cb1a",
           "corpro": "GMS",
-          "frame": "Everest",
-          "mech": "Scarlet Judicator"
+          "frame": "Chomolungma",
+          "mech": "Regent"
         },
         {
           "callsign": "Hitching Post",
           "alias": "Olivia Smith",
           "code": "98ca9616-044e-4f87-b89b-aae4eb3387ec///NDL-C-DEEP-STATION//6f572259-6946-41bf-931a-e0543709e892",
           "corpro": "GMS",
-          "frame": "Everest",
-          "mech": "Mayfly"
+          "frame": "Sagarmatha",
+          "mech": "Haypenny"
         },
         {
           "callsign": "Scylla",
           "alias": 'Argus Blayne',
           "code": "d1fdf62e-d81e-4e10-97c8-df3bc4860117///NDL-C-DEEP-STATION//5a4254aa-9fa2-42ca-a077-8f5bfd1e1ad3",
           "corpro": "GMS",
-          "frame": "Everest",
-          "mech": "Rio Bravo"
+          "frame": "Sagarmatha",
+          "mech": "Mightier Than The Pen"
         },
       ],
       "header": {
