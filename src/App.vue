672865bf3799f9de28de0a8729302f2a6d57c084
Techpet <tom_wells@hotmail.co.uk>
2024-02-18 11:05:57 +0000

    Bane Image

--- src/App.vue
+++ src/App.vue
@@ -143,7 +143,7 @@ export default {
           "code": "d1fdf62e-d81e-4e10-97c8-df3bc4860117///NDL-C-DEEP-STATION//5a4254aa-9fa2-42ca-a077-8f5bfd1e1ad3",
           "corpro": "GMS",
           "frame": "Sagarmatha",
-          "mech": "Mightier Than The Pen"
+          "mech": "Hecaton"
         },
       ],
       "header": {
